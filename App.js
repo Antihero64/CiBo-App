648ac88124ef1ab0047a77cc94a8cca2e6b7a1c5
Antihero64 <ridetheelements1@gmail.com>
2020-05-26 14:18:41 +0200

    fix backHandler

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 
+
 import React, { Component, useState } from "react";
-import { Alert, StyleSheet, Text, TouchableHighlight, View, Modal } from 'react-native';
+import { View, Text, BackHandler } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 
+export default class App extends Component {
 
-
-export default class MyWeb extends Component {
-  render() {
-    return (
-
-      <WebView
-        source={{ uri: 'https://circuit-board.de/forum/' }}
-        style={{ marginTop: 20 }}
-      />
-    );
-  }
+constructor(props) {
+    super(props);
+    this.WEBVIEW_REF = React.createRef();
 }
 
+componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+}
 
+componentWillUnmount() {
+  BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+}
+
+handleBackButton = ()=>{
+   this.WEBVIEW_REF.current.goBack();
+   return true;
+}
+
+onNavigationStateChange(navState) {
+  this.setState({
+    canGoBack: navState.canGoBack
+  });
+}
+
+render(){
+   return (
+    <WebView
+        source={{ uri: "https://www.circuit-board.de/forum" }}
+        ref={this.WEBVIEW_REF}
+        style={{ marginTop: 20 }}
+        onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+     />
+    )
+
+ }
+}
